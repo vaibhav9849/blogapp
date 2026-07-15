1e0908cd697595a184917a48b2ef4a6c359d998b
Vaibhav Shrivastava <134196260+vaibhav9849@users.noreply.github.com>
2026-07-15 12:35:28 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,17 @@ const nextConfig: NextConfig = {
         hostname: 'res.cloudinary.com'
       }
     ]
-  }
+  },
+ typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
